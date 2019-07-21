47ef86000641b3d3a1a0ac3875247d62b188a374
Asiqur Rahman <asikur22@gmail.com>
2019-07-21 07:05:42 +0600

    Updates

---
+++ precache-manifest.3460575760d0547a8f6c4c9175e7b5f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263c4a926f0ec375ea68e413f014085b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00408f6ca7889c06acc4",
+    "url": "/static/css/main.518d6e9f.chunk.css"
+  },
+  {
+    "revision": "ecd3f78b1b6aea042bef",
+    "url": "/static/js/2.804eb0bf.chunk.js"
+  },
+  {
+    "revision": "00408f6ca7889c06acc4",
+    "url": "/static/js/main.5cd15bff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
