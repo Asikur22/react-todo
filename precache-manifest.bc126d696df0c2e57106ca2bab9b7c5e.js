457644b6c5c7985799d00e4738e13528c2d60071
Asiqur Rahman <asikur22@gmail.com>
2019-07-21 07:15:16 +0600

    Updates

---
+++ precache-manifest.bc126d696df0c2e57106ca2bab9b7c5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c7a5f41e70afab8eace162d123725b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "00408f6ca7889c06acc4",
+    "url": "/react-todo/static/css/main.518d6e9f.chunk.css"
+  },
+  {
+    "revision": "ecd3f78b1b6aea042bef",
+    "url": "/react-todo/static/js/2.804eb0bf.chunk.js"
+  },
+  {
+    "revision": "00408f6ca7889c06acc4",
+    "url": "/react-todo/static/js/main.5cd15bff.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  }
+]);
